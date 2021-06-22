8701c1c62ea09a38ba84d4bca8f9dd179c46df1d
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-06-22 12:55:08 -0400

    removed redundant files

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var numbersArray = ['0','1','2','3','4','5','6','7','8','9'];
 
-var specialCharacters = ['U+0020','U+0021','U+0022','u+0023','u+0024','u+0025','u+0026','u+0027','u+0028','u+0029','u+002A','u+002B','u+002C','u+002D','u+002E','u+002F','u+003A','u+003B','u+003C','u+003D','u+003E','u+003F','u+0040','u+005B','u+005C','u+005D','u+005E','u+005F','u+0060','u+007B','U=007C','U=007D','U=007E'];
+var specialCharactersArray = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
 
 var password =[];
 
@@ -25,93 +25,54 @@ function writePassword() {
 
 function generatePassword() {
   var passlength = prompt("Length must be 8 to 128 characters. ");
-  var lowercase = confirm("Do you want to include lowercase letters in your pasword? ");
+  var lowerCase = confirm("Do you want to include lowercase letters in your pasword? ");
   var upperCase = confirm("Do you want to include uppercase letters in your password? ");
   var specialCharacters =confirm("Do you want to include special characters in your password? "); 
   var numbers = confirm("Do you want to include numbers in your password? ");
 
-  if (passlength < 8 || passlength >128) return
+  if (passlength < 7 || passlength >129) return
 for(var i = 0; i <=parseInt(passlength); i++) {
-  console.log("hi")
-  if (lowerCase) {
-    console.log("hi")
-    var random = lowerCase[Math.floor(Math.random()*lowerCase.length)];
-    console.log(random)
-    }
-
-    //if (passlength < 8 || passlength > 128) {
-  //return "Please select a number between 8 and 128";
-//} else {
-  //alert("Password length will be " + passlength + "characters.");}
-
-if (lowerCase === true) {
-  var randomLowerCase = lowerCase[Math.floor(Math.random()*lowerCase.length)];
-  console.log(randomLowerCase)
-  password.push(randomLowerCase);
+  
+  if (lowerCase === true) {
+    var randomLowerCase = lowerCaseArray[Math.floor(Math.random()*lowerCaseArray.length)];
+    console.log(randomLowerCase)
+    password.push(randomLowerCase);
   } 
     
   if (upperCase === true) {
-    var randomUpperCase = upperCase[Math.floor(Math.random()*upperCase.length)];
+    var randomUpperCase = upperCaseArray[Math.floor(Math.random()*upperCaseArray.length)];
     console.log(randomUpperCase)
     password.push(randomUpperCase);
   } 
   
   if (specialCharacters === true) {
-    var randomSpecialCharacters = specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+    var randomSpecialCharacters = specialCharactersArray[Math.floor(Math.random()*specialCharactersArray.length)];
     console.log(randomSpecialCharacters)
     password.push(randomSpecialCharacters);
     } 
 
     
   if (numbers === true) {
-    var randomNumbers = numbers[Math.floor(Math.random()*numbers.length)];
+    var randomNumbers = numbersArray[Math.floor(Math.random()*numbersArray.length)];
     console.log(randomNumbers)
-    password.push(numbers);
+    password.push(randomNumbers);
   }
   console.log(password) 
 }
 
+password = password.slice(0, parseInt(passlength));
+console.log(password)
 
+password = password.join('');
+console.log(password)
 
-//
-if (lowerCase === true) {
-  alert("Password will contain lowercase letters.");
-  } else {
-    alert("Password will not contain lowercase letters.");
+return password
+
 }
-    
-  if (upperCase === true) {
-  alert("Password will contain uppercase letters.");
-  } else {
-    alert("Password will not contain uppercase letters.");
-}
-  
-  if (specialCharacters === true) {
-  alert("Password will contain special characters.");
-    } else {
-      alert("Password will not containspecial characters");
-}
-    
-  if (numbers === true) {
-    alert("Password contain numbers.");
-  } else {
-    alert("password will not contain numbers.");
-}
-
-
-
-
-if (lowerCase) {console.log(lowercase)}
-if (upperCase) {console.log(upperCase)}
-if (specialCharacters) {console.log(specialCharacters)}
-if (numbers) {console.log(numbers)}
-}
-
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 
 
